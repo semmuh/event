3e06783ecbfaa0bc98462b8d4c7c2ce3d8a33416
sema <sema.muh@icloud.com>
2025-06-07 13:07:58 +0200

    Initial commit

---
+++ src/main/java/at/muharremoglu/event/VaadinProjektMuharremogluApplication.java
@@ -0,0 +1,13 @@
+package at.muharremoglu.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinProjektMuharremogluApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaadinProjektMuharremogluApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/muharremoglu/event/VaadinProjektMuharremogluApplicationTests.java
@@ -0,0 +1,13 @@
+package at.muharremoglu.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinProjektMuharremogluApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
